c8f981cb0bc4083163c21526e40400f4ca8f9c21
Hyeunjin KIM <kgcndjr121@naver.com>
2020-04-26 16:28:23 +0900

    3th file

---
+++ isempty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Study
+{
+    class EmptyCheck
+    {
+        public EmptyCheck(string _tempstr)
+        {
+            tempstr = _tempstr;
+        }
+        string tempstr;
+
+        public void getStr(string _tempstr)
+        {
+            tempstr = _tempstr;
+        }
+        public bool isempty()
+        {
+            return (tempstr == null || tempstr == "");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string tempstr = null;
+            EmptyCheck emptycheck = new EmptyCheck(tempstr);
+            bool isflag = emptycheck.isempty();
+            Console.WriteLine("{0}", isflag);
+
+            string tempstr2 = "";
+            emptycheck.getStr(tempstr2);
+            isflag = emptycheck.isempty();
+            Console.WriteLine("{0}", isflag);
+
+        }
+    }
+}
